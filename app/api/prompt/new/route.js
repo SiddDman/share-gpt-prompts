f5b8293a7909d0267015befbacf48fe6afd7a63d
Siddharth <siddharth12dhiman@gmail.com>
2024-06-01 23:23:02 +0530

    small fix

--- app/api/prompt/new/route.js
+++ app/api/prompt/new/route.js
@@ -5,16 +5,13 @@ export const POST = async (req) => {
     const { userId, prompt, tag } = await req.json()
     try {
         await connectToDB();
-        console.log("HUewjk")
         const newPrompt = new Prompt({
             creator: userId, prompt, tag
         })
 
         await newPrompt.save()
 
-        return new Response(JSON.stringify(newPrompt), {
-            status: 201
-        })
+        return new Response(JSON.stringify(newPrompt), { status: 201 })
 
     } catch (error) {
         return new Response("Failed to create a new prompt!!", { status: 500 })
