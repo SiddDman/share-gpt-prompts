39df1f49ae8bd198d77212b6fbb807d69fa2f05e
Siddharth <siddharth12dhiman@gmail.com>
2024-06-01 16:18:54 +0530

    added suspense while using search params

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -2,7 +2,7 @@
 
 import Form from "@components/Form";
 import { useRouter, useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 
 const EditPrompt = () => {
   const router = useRouter();
@@ -48,13 +48,15 @@ const EditPrompt = () => {
   };
 
   return (
-    <Form
-      type="Edit"
-      post={post}
-      setPost={setPost}
-      submitting={submitting}
-      handleSubmit={updatePrompt}
-    />
+    <Suspense fallback={<div>Loading ...</div>}>
+      <Form
+        type="Edit"
+        post={post}
+        setPost={setPost}
+        submitting={submitting}
+        handleSubmit={updatePrompt}
+      />
+    </Suspense>
   );
 };
 
